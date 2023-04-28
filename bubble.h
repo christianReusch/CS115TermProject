72c7ca4b14da7fccc5ea6b065a18fd69e019d914
christianReusch <130267096+christianReusch@users.noreply.github.com>
2023-04-27 18:41:47 -0700

    Update bubble.h

--- bubble.h
+++ bubble.h
@@ -8,9 +8,23 @@ public:
     ~bubbleSort();
 
     // sort function
-    void sort(int arr[], int n);
+    void sort(int arr[], int n) {
+        for (int i = 0; i < n; i++) {
+		//pass n times to check bubbles
+		    for (int j = n - 1; j > i; j--) {
+			    //compare adjacent elements
+			    if (arr[j] < arr[j - 1]) {
+				    swap(arr[j], arr[j - 1]);
+			    }
+		    }
+	    }
+    }
     // swap function
-    void swap(int *xp, int *yp);
+    void swap(int& xp, int& yp) {
+        int temp = xp;
+        xp = yp;
+        yp = temp;
+    }
 
     // print function
     void printArray(int arr[], int size);
