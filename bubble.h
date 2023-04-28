8cd9802b1c22d30e7e9f1ffc5cbb0d20ace7c0d6
christianReusch <130267096+christianReusch@users.noreply.github.com>
2023-04-27 18:58:50 -0700

    Update bubble.h

--- bubble.h
+++ bubble.h
@@ -1,33 +1,28 @@
 // bubble sort header file
-class bubbleSort {
-public:
-    // constructor
-    bubbleSort();
 
-    // destructor
-    ~bubbleSort();
-
-    // sort function
-    void sort(int arr[], int n) {
-        for (int i = 0; i < n; i++) {
-		//pass n times to check bubbles
-		    for (int j = n - 1; j > i; j--) {
-			    //compare adjacent elements
-			    if (arr[j] < arr[j - 1]) {
-				    swap(arr[j], arr[j - 1]);
-			    }
+// sort function
+void sort(int arr[], int n) {
+	for (int i = 0; i < n; i++) {
+	//pass n times to check bubbles
+	    for (int j = n - 1; j > i; j--) 
+		    //compare adjacent elements
+		    if (arr[j] < arr[j - 1]) {
+			    swap(arr[j], arr[j - 1]);
 		    }
-	    }
+	}
     }
-    // swap function
-    void swap(int& xp, int& yp) {
-        int temp = xp;
+}
+// swap function
+void swap(int& xp, int& yp) {
+	int temp = xp;
         xp = yp;
         yp = temp;
     }
-
-    // print function
-    void printArray(int arr[], int size);
-
-};
+  
+// print function
+void printArray(int arr[], int size) {
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	} cout << endl;
+}
 
